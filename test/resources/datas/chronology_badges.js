26ddfb60d3b474d4ba6bb3706dc585cf5745b83e
GoldBigDragon <dnwndugod642@naver.com>
2021-12-01 00:57:12 +0900

    Daily touch

--- test/resources/datas/chronology_badges.js
+++ test/resources/datas/chronology_badges.js
@@ -5,6 +5,7 @@ const text = {
 		"j": "龜南 小学校",
 		"c": "龜南 小學",
 		"r": "Kunam Начальная школа",
+		"date": "2002.03. ~ 2008.02.21.",
 		"background": "elementary_school.png",
 		"logo": "kunam.png"
 	},
@@ -14,6 +15,7 @@ const text = {
 		"j": "凡魚 中学校",
 		"c": "凡魚 中學",
 		"r": "Beomeo Средняя школа",
+		"date": "2008.03. ~ 2011.02.11.",
 		"background": "middle_school.png",
 		"logo": "beomeo.png"
 	},
@@ -23,7 +25,18 @@ const text = {
 		"j": "鎭海 高校",
 		"c": "鎭海 高學",
 		"r": "Jinhae Средняя школа",
+		"date": "2008.03.02. ~ 2014.02.08.",
 		"background": "high_school.png",
 		"logo": "jinhae.png"
+	},
+	"University":{
+		"e": "Youngsan University",
+		"k": "영산 대학교",
+		"j": "靈山 大学",
+		"c": "靈山 大學",
+		"r": "Youngsan Университет",
+		"date": "2014.03. ~ 2021.02.19.",
+		"background": "high_school.png",
+		"logo": "youngsan.png"
 	}
 }
