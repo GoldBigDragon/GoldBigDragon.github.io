95ac5ac1b9c404e9c7264af86c31af819c4fba28
GoldBigDragon <dnwndugod642@naver.com>
2021-12-05 11:19:33 +0900

    Daily touch

--- test/resources/datas/chronology_badges.js
+++ test/resources/datas/chronology_badges.js
@@ -1,42 +1,116 @@
 const text = {
 	"ELEMENTARY_SCHOOL": {
-		"e": "Kunam Elementary school",
-		"k": "구남 초등학교",
-		"j": "龜南 小学校",
-		"c": "龜南 小學",
-		"r": "Kunam Начальная школа",
+		"name":{
+			"e": "Kunam Elementary school",
+			"k": "구남 초등학교",
+			"j": "龜南 小学校",
+			"c": "龜南 小學",
+			"r": "Kunam Начальная школа",
+		},
+		"description":{
+			"e": "",
+			"k": "",
+			"j": "",
+			"c": "",
+			"r": "",
+		},
 		"date": "2002.03. ~ 2008.02.21.",
 		"background": "elementary_school.png",
 		"logo": "kunam.png"
 	},
 	"MIDDLE_SCHOOL":{
-		"e": "Beomeo Middle school",
-		"k": "범어 중학교",
-		"j": "凡魚 中学校",
-		"c": "凡魚 中學",
-		"r": "Beomeo Средняя школа",
+		"name":{
+			"e": "Beomeo Middle school",
+			"k": "범어 중학교",
+			"j": "凡魚 中学校",
+			"c": "凡魚 中學",
+			"r": "Beomeo Средняя школа",
+		},
+		"description":{
+			"e": "",
+			"k": "",
+			"j": "",
+			"c": "",
+			"r": "",
+		},
 		"date": "2008.03. ~ 2011.02.11.",
 		"background": "middle_school.png",
 		"logo": "beomeo.png"
 	},
 	"HIGH_SCHOOL":{
-		"e": "Jinhae High school",
-		"k": "진해 고등학교",
-		"j": "鎭海 高校",
-		"c": "鎭海 高學",
-		"r": "Jinhae Средняя школа",
+		"name":{
+			"e": "Jinhae High school",
+			"k": "진해 고등학교",
+			"j": "鎭海 高校",
+			"c": "鎭海 高學",
+			"r": "Jinhae Средняя школа",
+		},
+		"description":{
+			"e": "",
+			"k": "",
+			"j": "",
+			"c": "",
+			"r": "",
+		},
 		"date": "2008.03.02. ~ 2014.02.08.",
 		"background": "high_school.png",
 		"logo": "jinhae.png"
 	},
 	"University":{
-		"e": "Youngsan University",
-		"k": "영산 대학교",
-		"j": "靈山 大学",
-		"c": "靈山 大學",
-		"r": "Youngsan Университет",
+		"name":{
+			"e": "Youngsan University",
+			"k": "영산 대학교",
+			"j": "靈山 大学",
+			"c": "靈山 大學",
+			"r": "Youngsan Университет",
+		},
+		"description":{
+			"e": "",
+			"k": "",
+			"j": "",
+			"c": "",
+			"r": "",
+		},
 		"date": "2014.03. ~ 2021.02.19.",
 		"background": "high_school.png",
 		"logo": "youngsan.png"
+	},
+	"NURICOPS":{
+		"name":{
+			"e": "Nuricops",
+			"k": "누리캅스",
+			"j": "Nuricops",
+			"c": "Nuricops",
+			"r": "Nuricops",
+		},
+		"description":{
+			"e": "",
+			"k": "",
+			"j": "",
+			"c": "",
+			"r": "",
+		},
+		"date": "2014.03. ~ 2021.12.31.",
+		"background": "activity.png",
+		"logo": "nuricops.png"
+	},
+	"CORESECURITY":{
+		"name":{
+			"e": "Coresecurity",
+			"k": "코어시큐리티",
+			"j": "Coresecurity",
+			"c": "Coresecurity",
+			"r": "Coresecurity",
+		},
+		"description":{
+			"e": "",
+			"k": "",
+			"j": "",
+			"c": "",
+			"r": "",
+		},
+		"date": "2021.04.13. ~",
+		"background": "job.png",
+		"logo": "coresecurity.png"
 	}
 }
