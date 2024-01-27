ed3d3600caafa0c021fda57650fd50ce85b24afc
GoldBigDragon <dnwndugod642@naver.com>
2024-01-28 03:24:41 +0900

    Daily touch

--- resources/js/basic.js
+++ resources/js/basic.js
@@ -1,16 +1,16 @@
 function loadHeader() {
 	const headerElement = document.querySelector('#header');
 	if (headerElement) {
-		const headerElement = fetch('./templates/components/header.html');
+		const headerElement = fetch('/templates/components/header.html');
 		headerElement.then((res) => res.text()).then((text) => {
 			document.querySelector('#header').innerHTML = text;
 			const style = document.createElement("link");
 			style.rel = 'stylesheet';
-			style.href = './resources/css/header.css';
+			style.href = '/resources/css/header.css';
 			document.head.appendChild(style);
 			const script = document.createElement('script');
 			script.type = 'module';
-			script.src = './resources/js/header.js';
+			script.src = '/resources/js/header.js';
 			document.body.appendChild(script);
 		});
 	}
@@ -18,16 +18,16 @@ function loadHeader() {
 function loadFooter() {
 	const footerElement = document.querySelector('#footer');
 	if (footerElement) {
-		const footerElement = fetch('./templates/components/footer.html');
+		const footerElement = fetch('/templates/components/footer.html');
 		footerElement.then((res) => res.text()).then((text) => {
 			document.querySelector('#footer').innerHTML = text;
 			const style = document.createElement("link");
 			style.rel = 'stylesheet';
-			style.href = './resources/css/footer.css';
+			style.href = '/resources/css/footer.css';
 			document.head.appendChild(style);
 			const script = document.createElement('script');
 			script.type = 'module';
-			script.src = './resources/js/footer.js';
+			script.src = '/resources/js/footer.js';
 			document.body.appendChild(script);
 		});
 	}
