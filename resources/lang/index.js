b91b83c43fe0afea2dc43a7584cd65f7c9efac4e
GoldBigDragon <dnwndugod642@naver.com>
2024-03-31 14:41:41 +0900

    Daily touch

---
+++ resources/lang/index.js
@@ -0,0 +1,10 @@
+const INDEX_LANG = {
+	"new-post": {
+		"en": "New post",
+		"ko": "새 게시글",
+		"jp": "新規投稿",
+		"cn": "新帖子",
+		"ru": "Новое содержание"
+	}
+}
+LANGUAGE_OBJECT["INDEX_LANG"] = INDEX_LANG;
