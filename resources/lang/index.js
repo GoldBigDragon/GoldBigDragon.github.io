f24fe6fa9fda8ebc9cffbdaaeabf6559cb07f716
GoldBigDragon <dnwndugod642@naver.com>
2024-03-31 15:05:12 +0900

    Daily touch

--- resources/lang/index.js
+++ resources/lang/index.js
@@ -1,10 +1,38 @@
 const INDEX_LANG = {
-	"new-post": {
-		"en": "New post",
-		"ko": "새 게시글",
-		"jp": "新規投稿",
-		"cn": "新帖子",
-		"ru": "Новое содержание"
-	}
+	"name": {
+		"en": "Name",
+		"ko": "이름",
+		"jp": "名前",
+		"cn": "姓名",
+		"ru": "Имя"
+	},
+	"name-value": {
+		"en": "TaeRyong Kim",
+		"ko": "김태룡",
+		"jp": "金泰龍",
+		"cn": "金泰龍",
+		"ru": "TaeRyong Kim"
+	},
+	"country": {
+		"en": "Country",
+		"ko": "국가",
+		"jp": "国籍",
+		"cn": "国籍",
+		"ru": "Страна"
+	},
+	"country-value": {
+		"en": "South Korea",
+		"ko": "대한민국",
+		"jp": "韓国",
+		"cn": "韩国",
+		"ru": "Южная Корея"
+	},
+	"contact": {
+		"en": "Contact",
+		"ko": "연락처",
+		"jp": "連絡先",
+		"cn": "联系",
+		"ru": "Контакт"
+	},
 }
 LANGUAGE_OBJECT["INDEX_LANG"] = INDEX_LANG;
