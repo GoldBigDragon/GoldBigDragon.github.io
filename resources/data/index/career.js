0153af643cf672de54ac495d78812b7427fa5231
GoldBigDragon <dnwndugod642@naver.com>
2024-04-02 14:38:40 +0900

    Daily touch

---
+++ resources/data/index/career.js
@@ -0,0 +1,80 @@
+const DATA_CAREER = [
+	{
+		"company": "주식회사 피엘지",
+		"company-subtitle":"(PLZ Co.,Ltd.)",
+		"company-website":"https://pleaze.kr/",
+		"start-date":"2018-11-28",
+		"end-date":"2019-06-28",
+		"department":{
+			"e":"Research institute",
+			"k":"부설연구소",
+			"j":"附属研究所",
+			"c":"附属研究所",
+			"r":"Исследовательский институт"
+		},
+		"team": {
+			"e":"Development team",
+			"k":"개발팀",
+			"j":"開発チーム",
+			"c":"开发团队",
+			"r":"Команда разработчиков"
+		},
+		"task": {
+			"e":"Electronic document development",
+			"k":"전자 문서 개발",
+			"j":"電子文書開発",
+			"c":"电子文档开发",
+			"r":"Разработка электронного документа"
+		},
+		"position": {
+			"e":"Intern",
+			"k":"인턴",
+			"j":"インターン",
+			"c":"实习生",
+			"r":"Стажер"
+		},
+		"logo":"/img/index/carrer/PLZ.png"
+	},
+	{
+		"company": "(주)코어시큐리티",
+		"company-subtitle":"(Coresecurity)",
+		"company-website":"https://coresec.co.kr/",
+		"start-date":"2021-04-12",
+		"end-date":{
+			"e":"Currently working",
+			"k":"재직 중",
+			"j":"今、働いている",
+			"c":"正在工作",
+			"r":"Работает в настоящее время"
+		},
+		"department":{
+			"e":"Security Technology Research Institute",
+			"k":"보안기술연구소",
+			"j":"保安技術研究所",
+			"c":"安全技术研究所",
+			"r":"Научно-исследовательский институт технологий безопасности"
+		},
+		"team": {
+			"e":"Threat Technology Research Team",
+			"k":"위협기술연구팀",
+			"j":"脅威技術研究チーム",
+			"c":"威胁技术研究团队",
+			"r":"Группа исследования технологий угроз"
+		},
+		"task": {
+			"e":"",
+			"k":"",
+			"j":"",
+			"c":"",
+			"r":""
+		},
+		"position": {
+			"e":"Assistant Research Engineer",
+			"k":"주임 연구원",
+			"j":"主任硏究員",
+			"c":"主任硏究員",
+			"r":"Помощник инженера-исследователя"
+		},
+		"logo":"/img/index/carrer/coresecurity.png"
+	}
+];
