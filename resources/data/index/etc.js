00710a9e9ea9cfba8d1f3e4def3c91b57a164801
GoldBigDragon <dnwndugod642@naver.com>
2024-04-07 23:48:18 +0900

    Daily touch

--- resources/data/index/etc.js
+++ resources/data/index/etc.js
@@ -1,6 +1,303 @@
 const DATA_ETC = [
 	{
 		"award-name":{
+			"en":"Appreciation plaque",
+			"ko":"감사패",
+			"jp":"感謝楯",
+			"cn":"感谢牌",
+			"ru":"Благодарственная табличка"
+		},
+		"award-date":"2022-12-23",
+		"awarding-organization":{
+			"en":"Korean National Police Agency - Ulsan Provincial",
+			"ko":"울산지방경찰청",
+			"jp":"蔚山警察庁",
+			"cn":"蔚山警察厅",
+			"ru":"Корейское национальное полицейское управление - провинциальное Ulsan"
+		},
+		"reason":{
+			"en":"Excellent activity in reporting harmful sites in 2022",
+			"ko":"2022년 유해 사이트 신고 활동 우수",
+			"jp":"2022年有害サイト届出活動優秀",
+			"cn":"2022年报告有害网站的出色活动",
+			"ru":"Отличная активность по сообщению о вредоносных сайтах в 2022 г."
+		},
+		"logo":"/resources/img/index/logo/etc/knpa.png",
+		"logo-title":{
+			"en":"Appreciation plaque",
+			"ko":"감사패",
+			"jp":"感謝楯",
+			"cn":"感谢牌",
+			"ru":"Благодарственная табличка"
+		},
+		"proof":"/resources/img/index/proof/etc/2022_12_23_knpa.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2020-12-24",
+		"awarding-organization":{
+			"en":"Korean National Police Agency",
+			"ko":"경찰청",
+			"jp":"警察庁",
+			"cn":"警察厅",
+			"ru":"Корейское национальное полицейское управление"
+		},
+		"reason":{
+			"en":"<ul><li>Excellent reporting activity on harmful sites throughout 2020</li><li>Cumulative 100,000 reports</li></ul>",
+			"ko":"<ul><li>2020년 전체 기간 유해 사이트 신고 활동 우수</li><li>누적 10만 건 신고</li></ul>",
+			"jp":"<ul><li>2020年全期間有害サイト届出活動優秀</li><li>累積10万件の届出</li></ul>",
+			"cn":"<ul><li>2020年报告有害网站的出色活动</li><li>累计 100,000 份报告</li></ul>",
+			"ru":"<ul><li>Отличная активность по информированию о вредоносных сайтах на протяжении 2020 года.</li><li>Всего 100 000 отчетов</li></ul>"
+		},
+		"logo":"/resources/img/index/logo/etc/knpa.png",
+		"logo-title":{
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"proof":"/resources/img/index/proof/etc/2020_12_24_knpa.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2020-10-21",
+		"awarding-organization":{
+			"en":"Korean National Police Agency - Ulsan Provincial",
+			"ko":"울산지방경찰청",
+			"jp":"蔚山警察庁",
+			"cn":"蔚山警察厅",
+			"ru":"Корейское национальное полицейское управление - провинциальное Ulsan"
+		},
+		"reason":{
+			"en":"Police Day Celebration",
+			"ko":"경찰의날 기념",
+			"jp":"警察の日記念",
+			"cn":"警察日庆祝活动",
+			"ru":"Празднование Дня полиции"
+		},
+		"logo":"/resources/img/index/logo/etc/knpa.png",
+		"logo-title":{
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"proof":"/resources/img/index/proof/etc/2020_10_21_knpa_ulsan.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2020-06-25",
+		"awarding-organization":{
+			"en":"Korean National Police Agency - Ulsan Provincial",
+			"ko":"울산지방경찰청",
+			"jp":"蔚山警察庁",
+			"cn":"蔚山警察厅",
+			"ru":"Корейское национальное полицейское управление - провинциальное Ulsan"
+		},
+		"reason":{
+			"en":"Excellent activity in reporting harmful sites in April 2020",
+			"ko":"2020년 4월 유해 사이트 신고 활동 우수",
+			"jp":"2020年4月有害サイト届出活動優秀",
+			"cn":"2020年4月报告有害网站的出色活动",
+			"ru":"Отличная активность по сообщению о вредоносных сайтах в апреле 2020 г."
+		},
+		"logo":"/resources/img/index/logo/etc/knpa.png",
+		"logo-title":{
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"proof":"/resources/img/index/proof/etc/2020_06_25_knpa_ulsan.jpg"
+	},
+	{
+		"award-name":{
+			"en":"'Find TS security loopholes!' Vulnerability Report Contest - Excellence Award",
+			"ko":"'TS 보안 허점을 찾아라!' 취약점 신고 대회 - 우수상",
+			"jp":"「TSセキュリティホールを見つけよう！」 脆弱性の報告大会 - 優秀賞",
+			"cn":"'找到 TS 安全漏洞!' 漏洞报告大赛 - 优秀奖",
+			"ru":"«Найдите лазейки в безопасности TS!» Конкурс отчетов об уязвимостях – награда за выдающиеся достижения"
+		},
+		"award-date":"2019-12-16",
+		"awarding-organization":{
+			"en":"Korea Transportation Safety Authority",
+			"ko":"한국교통안전공단",
+			"jp":"韓国交通安全公団",
+			"cn":"韓國交通安全公團",
+			"ru":"Управление транспортной безопасности Кореи"
+		},
+		"reason":{
+			"en":"Reported 24 vulnerabilities on website.",
+			"ko":"홈페이지 내 취약점 24건 신고",
+			"jp":"ホームページの脆弱性24件を報告",
+			"cn":"报告网站上存在 24 个漏洞",
+			"ru":"Сообщено о 24 уязвимостях на сайте"
+		},
+		"logo":"/resources/img/index/logo/etc/ktsa.png",
+		"logo-title":{
+			"en":"Vulnerability Report Contest<br>Excellence Award",
+			"ko":"취약점 신고 대회<br>우수상",
+			"jp":"脆弱性の報告大会<br>優秀賞",
+			"cn":"漏洞报告大赛<br>优秀奖",
+			"ru":"Конкурс отчетов об уязвимостях<br>награда за выдающиеся достижения"
+		},
+		"proof":"/resources/img/index/proof/etc/2019_12_16_ktsa.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2018-12-11",
+		"awarding-organization":{
+			"en":"Game Rating and Administration Committee",
+			"ko":"게임물관리위원회",
+			"jp":"ゲーム物管理委員会",
+			"cn":"游戏产业管理委员会",
+			"ru":"Комитет по рейтингу и администрированию игры"
+		},
+		"reason":{
+			"en":"Excellent reporting activity on illegal games throughout 2018",
+			"ko":"2018년 전체 기간 불법게임물 신고 활동 우수",
+			"jp":"2018年全期間違法ゲーム物申告活動優秀",
+			"cn":"2018年举报非法游戏产品的出色活动",
+			"ru":"Отличная репортажная активность о нелегальных играх в 2018 году."
+		},
+		"logo":"/resources/img/index/logo/etc/grac.png",
+		"logo-title":{
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"proof":"/resources/img/index/proof/etc/2018_12_11_grac.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2018-11-29",
+		"awarding-organization":{
+			"en":"Korean National Police Agency",
+			"ko":"경찰청",
+			"jp":"警察庁",
+			"cn":"警察厅",
+			"ru":"Корейское национальное полицейское управление"
+		},
+		"reason":{
+			"en":"Excellent reporting activity on harmful sites throughout 2018",
+			"ko":"2018년 전체 기간 유해 사이트 신고 활동 우수",
+			"jp":"2018年全期間有害サイト届出活動優秀",
+			"cn":"2018年报告有害网站的出色活动",
+			"ru":"Отличная активность по информированию о вредоносных сайтах на протяжении 2018 года."
+		},
+		"logo":"/resources/img/index/logo/etc/knpa.png",
+		"logo-title":{
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"proof":"/resources/img/index/proof/etc/2018_11_29_knpa_2.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2018-11-29",
+		"awarding-organization":{
+			"en":"Korean National Police Agency",
+			"ko":"경찰청",
+			"jp":"警察庁",
+			"cn":"警察厅",
+			"ru":"Корейское национальное полицейское управление"
+		},
+		"reason":{
+			"en":"Excellent activity in reporting harmful sites in May 2018",
+			"ko":"2018년 5월 유해 사이트 신고 활동 우수",
+			"jp":"2018年5月有害サイト届出活動優秀",
+			"cn":"2018 年 5 月报告有害网站的出色活动",
+			"ru":"Отличная активность по сообщению о вредоносных сайтах в мае 2018 г."
+		},
+		"logo":"/resources/img/index/logo/etc/knpa.png",
+		"logo-title":{
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"proof":"/resources/img/index/proof/etc/2018_11_29_knpa.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of commendation for exemplary social service agent",
+			"ko":"모범 사회복무요원 표창장",
+			"jp":"模範社会服務要員表彰状",
+			"cn":"模范社会服务代理人荣誉证书",
+			"ru":"Почетная грамота образцовому работнику социальной службы"
+		},
+		"award-date":"2018-11-16",
+		"awarding-organization":{
+			"en":"Gyeongnam Regional Military Manpower Administration",
+			"ko":"경남지방 병무청",
+			"jp":"慶南地方兵務庁",
+			"cn":"慶南地方兵务厅",
+			"ru":"Региональное управление кадровых ресурсов Gyeongnam"
+		},
+		"reason":{
+			"en":"Perform Yangsan City Hall Tax Collection Division Arrears Mobile Team work",
+			"ko":"양산시청 징수과 체납기동팀 업무 수행",
+			"jp":"梁山市役所徴収課滞納機動チーム業務遂行",
+			"cn":"履行职责梁山市政厅税收部门欠款机动队",
+			"ru":"Выполнение работы мобильной группы отдела по сбору налогов мэрии Янсана"
+		},
+		"logo":"/resources/img/index/logo/etc/military_manpower_administration.png",
+		"logo-title":{
+			"en":"Service commendation certificate",
+			"ko":"표창장",
+			"jp":"表彰状",
+			"cn":"荣誉证书",
+			"ru":"Благодарственная грамота за службу"
+		},
+		"proof":"/resources/img/index/proof/etc/2018_11_16_military_manpower_administration_gyeongnam_regional.jpg"
+	},
+	{
+		"award-name":{
 			"en":"Cyber Safety Public Idea Contest - Encouragement Award",
 			"ko":"사이버안전 대국민 아이디어 공모전 - 장려상",
 			"jp":"サイバー安全大国民のアイデア公募展 - 奨励賞",
@@ -52,7 +349,7 @@ const DATA_ETC = [
 			"en":"Excellent activity in reporting harmful sites in May 2016",
 			"ko":"2016년 5월 유해 사이트 신고 활동 우수",
 			"jp":"2016年5月有害サイト届出活動優秀",
-			"cn":"2016 年 5 月报告有害网站的出色活动",
+			"cn":"2016年5月报告有害网站的出色活动",
 			"ru":"Отличная активность по сообщению о вредоносных сайтах в мае 2016 г."
 		},
 		"logo":"/resources/img/index/logo/etc/knpa.png",
@@ -102,8 +399,8 @@ const DATA_ETC = [
 		"award-name":{
 			"en":"Community service commendation certificate",
 			"ko":"사회봉사 표창장",
-			"jp":"社会貢献表彰",
-			"cn":"社区服务表彰",
+			"jp":"社会貢献表彰状",
+			"cn":"社区服务荣誉证书",
 			"ru":"Благодарность за общественные работы"
 		},
 		"award-date":"2023-08-11",
@@ -126,7 +423,7 @@ const DATA_ETC = [
 			"en":"Red Cross Youth<br>commendation certificate",
 			"ko":"RCY 사회봉사 표창장",
 			"jp":"RCY 表彰状",
-			"cn":"RCY 表彰",
+			"cn":"RCY 荣誉证书",
 			"ru":"Благодарственная грамота<br>молодежи Красного Креста."
 		},
 		"proof":"/resources/img/index/proof/etc/2012_10_27_rcy.jpg"
@@ -135,8 +432,8 @@ const DATA_ETC = [
 		"award-name":{
 			"en":"Community service commendation certificate",
 			"ko":"사회봉사 표창장",
-			"jp":"社会貢献表彰",
-			"cn":"社区服务表彰",
+			"jp":"社会貢献表彰状",
+			"cn":"社区服务荣誉证书",
 			"ru":"Благодарность за общественные работы"
 		},
 		"award-date":"2023-08-11",
@@ -159,7 +456,7 @@ const DATA_ETC = [
 			"en":"Red Cross Youth<br>commendation certificate",
 			"ko":"RCY 사회봉사 표창장",
 			"jp":"RCY 表彰状",
-			"cn":"RCY 表彰",
+			"cn":"RCY 荣誉证书",
 			"ru":"Благодарственная грамота<br>молодежи Красного Креста."
 		},
 		"proof":"/resources/img/index/proof/etc/2011_11_11_rcy.jpg"
