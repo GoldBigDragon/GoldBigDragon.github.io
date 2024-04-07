2ca35a8943dcfdff264a2358a523633adb3a1318
GoldBigDragon <dnwndugod642@naver.com>
2024-04-07 18:58:02 +0900

    Daily touch

--- resources/data/index/etc.js
+++ resources/data/index/etc.js
@@ -1,34 +1,100 @@
 const DATA_ETC = [
 	{
 		"award-name":{
-			"en":"",
-			"ko":"",
-			"jp":"",
-			"cn":"",
-			"ru":""
+			"en":"Cyber Safety Public Idea Contest - Encouragement Award",
+			"ko":"사이버안전 대국민 아이디어 공모전 - 장려상",
+			"jp":"サイバー安全大国民のアイデア公募展 - 奨励賞",
+			"cn":"网络安全公益创意大赛-鼓励奖",
+			"ru":"Конкурс общественных идей кибербезопасности – Поощрительная премия"
 		},
-		"award-date":"2023-08-11",
+		"award-date":"2018-05-25",
 		"awarding-organization":{
-			"en":"",
-			"ko":"",
-			"jp":"",
-			"cn":"",
-			"ru":""
+			"en":"Korean National Police Agency Cyber Bureau",
+			"ko":"경찰청 사이버안전국",
+			"jp":"警察庁サイバー安全局",
+			"cn":"警察厅网络保安局",
+			"ru":"Бюро кибербезопасности Корейское Национального полицейского агентства"
 		},
 		"reason":{
-			"en":"",
-			"ko":"",
-			"jp":"",
-			"cn":"",
-			"ru":""
+			"en":"Policy proposals to ensure cyberspace safety",
+			"ko":"사이버공간 안전 확보를 위한 정책 제안",
+			"jp":"サイバー空間の安全確保のための政策提案",
+			"cn":"提出确保网络空间安全的政策",
+			"ru":"Предложения по политике по обеспечению безопасности киберпространства"
+		},
+		"logo":"/resources/img/index/logo/etc/knpa_cyber_bureau.png",
+		"logo-title":{
+			"en":"Policy Proposal<br>Encouragement Award",
+			"ko":"정책 제안<br>장려상",
+			"jp":"政策提案<br>奨励賞",
+			"cn":"政策提案<br>鼓励奖",
+			"ru":"Политическое предложение<br>Поощрительная премия"
+		},
+		"proof":"/resources/img/index/proof/etc/2018_05_25_knpa_cyber_bureau.png"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2016-06-30",
+		"awarding-organization":{
+			"en":"Korean National Police Agency",
+			"ko":"경찰청",
+			"jp":"警察庁",
+			"cn":"警察厅",
+			"ru":"Корейское национальное полицейское управление"
+		},
+		"reason":{
+			"en":"Excellent activity in reporting harmful sites in May 2016",
+			"ko":"2016년 5월 유해 사이트 신고 활동 우수",
+			"jp":"2016年5月有害サイト届出活動優秀",
+			"cn":"2016 年 5 月报告有害网站的出色活动",
+			"ru":"Отличная активность по сообщению о вредоносных сайтах в мае 2016 г."
 		},
 		"logo":"/resources/img/index/logo/etc/knpa.png",
 		"logo-title":{
-			"en":"",
-			"ko":"",
-			"jp":"",
-			"cn":"",
-			"ru":""
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"proof":"/resources/img/index/proof/etc/2016_06_30_knpa.jpg"
+	},
+	{
+		"award-name":{
+			"en":"Certificate of Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
+		},
+		"award-date":"2016-04-27",
+		"awarding-organization":{
+			"en":"Korean National Police Agency - Ulsan Provincial",
+			"ko":"울산지방경찰청",
+			"jp":"蔚山警察庁",
+			"cn":"蔚山警察厅",
+			"ru":"Корейское национальное полицейское управление - провинциальное Ulsan"
+		},
+		"reason":{
+			"en":"Volunteer work to report harmful sites",
+			"ko":"유해 사이트 신고 활동",
+			"jp":"有害サイトの申告活動",
+			"cn":"有害网站报告活动",
+			"ru":"Волонтерская работа по сообщению о вредоносных сайтах"
+		},
+		"logo":"/resources/img/index/logo/etc/knpa.png",
+		"logo-title":{
+			"en":"Certificate of<br>Appreciation",
+			"ko":"감사장",
+			"jp":"感謝状",
+			"cn":"感谢状",
+			"ru":"Благодарность"
 		},
 		"proof":"/resources/img/index/proof/etc/2016_04_27_knpa_ulsan.jpg"
 	},
@@ -79,7 +145,7 @@ const DATA_ETC = [
 			"ko":"대한적십자사 경상남도지사",
 			"jp":"韓国赤十字社慶尚南道知事",
 			"cn":"韩国红十字会庆尚南道知事",
-			"ru":"Губернатор Корейского Красного Креста в Кёнсаннамдо"
+			"ru":"Руководитель Gyeongsangnam-do Корейского Красного Креста"
 		},
 		"reason":{
 			"en":"Participating in volunteer activities",
