cf9dbe8fc10f460310603da7e5ea50bb55faf152
GoldBigDragon <dnwndugod642@naver.com>
2024-04-08 00:07:26 +0900

    Daily touch

---
+++ resources/data/cartoon/cartoon_list.js
@@ -0,0 +1,32 @@
+const CARTOON_LIST = [
+	{
+		"title":{
+			"en":"Daily cartoon",
+			"ko":"일상 만화",
+			"jp":"",
+			"cn":"",
+			"ru":""
+		},
+		"cover": "/resources/img/cartoon/daily/cover.png"
+	},
+	{
+		"title":{
+			"en":"Developer messenger chat",
+			"ko":"개발자 메신저 대화",
+			"jp":"",
+			"cn":"",
+			"ru":""
+		},
+		"cover": "/resources/img/cartoon/developer/cover.png"
+	},
+	{
+		"title":{
+			"en":"Game friend chat",
+			"ko":"게임 지인 대화",
+			"jp":"",
+			"cn":"",
+			"ru":""
+		},
+		"cover": "/resources/img/cartoon/gamer/cover.png"
+	}
+];
---
+++ resources/lang/cartoon.js
@@ -0,0 +1,10 @@
+const CARTOON_LANG = {
+	"name": {
+		"en": "Name:",
+		"ko": "이름:",
+		"jp": "名前:",
+		"cn": "姓名:",
+		"ru": "Имя:"
+	}
+}
+LANGUAGE_OBJECT["CARTOON_LANG"] = CARTOON_LANG;
