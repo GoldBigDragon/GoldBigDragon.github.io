d15cab7d97395fc8d728789cab66014476bd9178
GoldBigDragon <dnwndugod642@naver.com>
2024-04-11 03:06:33 +0900

    Daily touch

--- resources/lang/cartoon.js
+++ resources/lang/cartoon.js
@@ -1,10 +1,10 @@
 const CARTOON_LANG = {
 	"move": {
-		"en": "<i class='fa-solid fa-binoculars'></i>&nbsp;Go",
-		"kr": "<i class='fa-solid fa-binoculars'></i>&nbsp;이동",
-		"jp": "<i class='fa-solid fa-binoculars'></i>&nbsp;移動",
-		"cn": "<i class='fa-solid fa-binoculars'></i>&nbsp;移动",
-		"ru": "<i class='fa-solid fa-binoculars'></i>&nbsp;Идти"
+		"en": "<i class='fa-solid fa-binoculars'></i>",
+		"kr": "<i class='fa-solid fa-binoculars'></i>",
+		"jp": "<i class='fa-solid fa-binoculars'></i>",
+		"cn": "<i class='fa-solid fa-binoculars'></i>",
+		"ru": "<i class='fa-solid fa-binoculars'></i>"
 	}
 }
 LANGUAGE_OBJECT["CARTOON_LANG"] = CARTOON_LANG;
