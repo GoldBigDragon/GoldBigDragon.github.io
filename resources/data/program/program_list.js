da4dc66a66caf5382793493172f9ba38fbe1a151
GoldBigDragon <dnwndugod642@naver.com>
2024-04-14 18:30:52 +0900

    Daily touch

--- resources/data/program/program_list.js
+++ resources/data/program/program_list.js
@@ -1,2 +1,187 @@
 const PROGRAM_LIST = [
+	{
+		"name": {
+			"en":"GoldBigDragon RPG",
+			"kr":"GoldBigDragon RPG",
+			"jp":"GoldBigDragon RPG",
+			"cn":"GoldBigDragon RPG",
+			"ru":"GoldBigDragon RPG"
+		},
+		"description": {
+			"en":"Minecraft RPG plugin",
+			"kr":"마인크래프트 RPG 플러그인",
+			"jp":"Minecraft RPGプラグインです",
+			"cn":"Minecraft 角色扮演游戏插件",
+			"ru":"RPG Плагин Minecraft"
+		},
+		"tag": [
+			"Minecraft",
+			"Plugin",
+			"RPG",
+			"Game",
+			"Java",
+			"Spigot",
+			"1.12.2"
+		],
+		"language": "Java 8",
+		"download": "/resources/files/program/GoldBigDragon RPG/Advanced/GoldBigDragonRPG_Advanced_2020_10_24_01_30.zip",
+		"github": "https://github.com/GoldBigDragon/GoldBigDragonRPG",
+		"youtube": "https://youtu.be/slzghzo4My4",
+		"icon": "/resources/img/program/GoldBigDragon RPG/icon_advanced.png",
+		"created-at": "2014-08-21",
+		"updated-at": "2020-10-24",
+	},
+	{
+		"name": {
+			"en":"Minecraft Resourcepack Compactor",
+			"kr":"마인크래프트 리소스팩 압축기",
+			"jp":"Minecraft Resourcepack 圧縮機",
+			"cn":"Minecraft Resourcepack压缩器",
+			"ru":"Компрессор Minecraft Resourcepack"
+		},
+		"description": {
+			"en":"Reduce the capacity of image files and .json files in Minecraft resourcepacks.",
+			"kr":"마인크래프트 리소스팩 속 이미지 파일과 .json 파일의 용량을 줄여줍니다.",
+			"jp":"Minecraft Resourcepackの中のイメージファイルと.jsonファイルの容量を減らします。",
+			"cn":"减少 Minecraft Resourcepack 中的映像文件和 .json 文件的容量 。",
+			"ru":"Уменьшите емкость файлов изображений и файлов .json в пакетах ресурсов Minecraft."
+		},
+		"tag": [
+			"Minecraft",
+			"Resourcepack",
+			"Compress",
+			"Image",
+			"Json",
+			"Tool",
+			"Java"
+		],
+		"language": "Java 8",
+		"download": "/resources/files/program/Minecraft Resourcepack Compactor/Minecraft Resourcepack Compactor.zip",
+		"github": "https://github.com/GoldBigDragon/MinecraftResourcepackCompactor",
+		"youtube": "https://youtu.be/62uqfHo_SQE",
+		"icon": "/resources/img/program/Minecraft Resourcepack Compactor/icon.png",
+		"created-at": "2018-02-06",
+		"updated-at": "2018-02-06",
+	},
+	{
+		"name": {
+			"en":"Minecraft Resourcepack Sound Player",
+			"kr":"마인크래프트 리소스팩 소리 재생기",
+			"jp":"Minecraft Resourcepack 音再生機",
+			"cn":"Minecraft Resourcepack 声音播放器",
+			"ru":"Плейер звука Minecraft Resourcepack"
+		},
+		"description": {
+			"en":"Play the .ogg file in the Minecraft resourcepack to the player.",
+			"kr":"마인크래프트 리소스팩 속 .ogg 파일을 플레이어에게 재생시킵니다.",
+			"jp":"Minecraft Resourcepack の中の.ogg ファイルをプレーヤーに再生させます。",
+			"cn":"将 Minecraft Resourcepack 中的.ogg 文件播放给玩家 。",
+			"ru":"Воспроизведение файла .ogg в пакете ресурсов Minecraft игроку."
+		},
+		"tag": [
+			"Minecraft",
+			"Plugin",
+			"Resourcepack",
+			"Tool",
+			"Java"
+			"Spigot"
+		],
+		"language": "Java 8",
+		"download": "/resources/files/program/Minecraft Resourcepack Sound Player/MinecraftResourcepackSoundPlayer.jar",
+		"github": "https://github.com/GoldBigDragon/Minecraft-Resourcepack-Sound-Player",
+		"video": "https://github.com/GoldBigDragon/Minecraft-Resourcepack-Sound-Player/raw/main/GLOBAL_README/06%20plugin%20test.mp4",
+		"icon": "/resources/img/program/Minecraft Resourcepack Sound Player/icon.png",
+		"created-at": "2019-10-15",
+		"updated-at": "2023-01-28",
+	},
+	{
+		"name": {
+			"en":"NuriCHAIN",
+			"kr":"NuriCHAIN",
+			"jp":"NuriCHAIN",
+			"cn":"NuriCHAIN",
+			"ru":"NuriCHAIN"
+		},
+		"description": {
+			"en":"To respond to the increasing illegal harmful information, the Cyber Bureau has created NuriCops group with police and citizens in 2007. I have been with NuriCops since 2014. And mainly monitoring and reporting harmful sites. During the monitoring activities, I noticed that there are patterns and trends on harmful sites. And since the summer of 2016, I developed harmful site search crawlers on the premise that 'harmful sites have patterns.'",
+			"kr":"증가하는 불법 유해정보에 적극 대응하기 위해 사이버안전국은 2007년 부터 경찰과 시민으로 결성된 사이버 명예경찰 집단인 누리캅스(NuriCops)를 창설하였습니다. 필자는 누리캅스 일원으로 써 2014년 부터 현재 까지 활동하고 있으며, 주로 유해 사이트 모니터링 및 신고활동을 하고 있습니다. 모니터링활동 중, 유해사이트는 패턴과 유행을 가지는 양상을 확인하여 '유해사이트는 패턴을 가졌다'를 전제로 2016년 여름부터 유해사이트 탐색 크롤러(Crawler)를 제작하게 되었습니다.",
+			"jp":"増加する不法有害情報に積極的に対応するためにサイバー安全局は2007年から警察と市民で結成されたサイバー名誉警察集団であるヌリコップス(NuriCops)を創設しました。 筆者はヌリカプスの一員として2014年から現在まで活動しており、主に有害サイトのモニタリングおよび申告活動を行っています。 モニタリング活動中、有害サイトはパターンと流行を持つ様相を確認し、「有害サイトはパターンを持つ」ことを前提に2016年夏から有害サイト探索クローラ(Crawler)を製作することになりました。",
+			"cn":"为了应对日益严重的非法有害信息,网络局于2007年成立了由警察和市民组成的NuriCops集团。 我自2014年以来一直在NuriCops工作。 主要是对有害场所进行监测和报告。 在监测活动中，我注意到有害网站存在模式和趋势。 自2016年夏天以来，我在'有害网站有模式'的前提下开发了有害的网站搜索爬虫。",
+			"ru":"Чтобы ответить на растущую незаконную вредную информацию, кибербюро создало группу NuriCops с полицией и гражданами в 2007 году. Я работаю с NuriCops с 2014 года. И в основном мониторинг и отчетность вредных сайтов. В ходе мониторинга я заметил, что существуют модели и тенденции на вредных сайтах. И с лета 2016 года я разработал вредные поисковые маршрутизаторы с предпосылкой, что «нарушительные сайты имеют узоры»."
+		},
+		"tag": [
+			"NuriCops",
+			"Crime prevention",
+			"Crawler",
+			"Tool",
+			"Java",
+			"Java FX"
+		],
+		"language": "Java 8",
+		"download": "/resources/files/program/NuriCHAIN/NuriCHAIN_2019_11_25_21_06.zip",
+		"document": "/resources/files/program/NuriCHAIN/NuriCHAIN_Report.pdf",
+		"youtube": "https://youtu.be/BkHq69RD9l4",
+		"icon": "/resources/img/program/NuriCHAIN/icon.png",
+		"created-at": "2016-10-19",
+		"updated-at": "2020-03-25",
+	},
+	{
+		"name": {
+			"en":"Science Convergence Police Training Program",
+			"kr":"범죄 정보 과학융합 훈련 프로그램",
+			"jp":"犯罪情報科学融合訓練プログラム",
+			"cn":"犯罪信息科学融合训练计划",
+			"ru":"Программа обучения конвергенции уголовной информации"
+		},
+		"description": {
+			"en":"When crime data and weights are entered, it is a program that identifies areas of concern for crime by making statistics on input values.",
+			"kr":"범죄 데이터와 가중치를 입력하면, 입력 값들에 대한 통계를 내어 범죄 발생 우려 지역을 확인하는 프로그램입니다.",
+			"jp":"犯罪データと加重値を入力すると、入力値に対する統計を出して犯罪発生の恐れがある地域を確認するプログラムです。",
+			"cn":"输入犯罪数据和权重值后,对输入值进行统计,确认犯罪发生可能性地区的程序。",
+			"ru":"При вводе преступных данных и весов, это программа, которая определяет области, озабоченные преступностью, путем создания статистики по входным значениям."
+		},
+		"tag": [
+			"Crime prevention",
+			"Tool",
+			"Java",
+			"Java FX"
+		],
+		"language": "Java 8",
+		"download": "/resources/files/program/Science Convergence Police Training Program/Science Convergence Police Training Program.zip",
+		"document": "/resources/files/program/Science Convergence Police Training Program/Science Convergence Police Training Program Development Report.pdf",
+		"youtube": "https://youtu.be/4Dzo4vluiL4",
+		"icon": "/resources/img/program/Science Convergence Police Training Program/icon.png",
+		"created-at": "2020-03-20",
+		"updated-at": "2020-06-23",
+	},
+	{
+		"name": {
+			"en":"ARIA Algorithm Ransomware",
+			"kr":"ARIA 알고리즘 랜섬웨어",
+			"jp":"ARIA アルゴリズム ランサムウェア",
+			"cn":"ARIA 算法勒索软件",
+			"ru":"Алгоритм-вымогатель ARIA"
+		},
+		"description": {
+			"en":"This is a commentary on the 2020 Digital Forensics Challenge questions.",
+			"kr":"2020년도 Digital Forensics Challenge 출제 문제에 대한 해설입니다.",
+			"jp":"令和2年度 Digital Forensics Challenge出題問題についての解説です。",
+			"cn":"这是对2020年Digital Forensics Challenge出题问题的解说。",
+			"ru":"Это комментарий к вопросу Digital Forensics Challenge 2020 года."
+		},
+		"tag": [
+			"Encryption",
+			"Decryption",
+			"ARIA",
+			"Ransomware",
+			"Tool",
+			"Java"
+		],
+		"language": "Java 8",
+		"download": "/resources/files/program/ARIA Algorithm Ransomware/DFC2020_301_ARIA_RANSOMWARE.zip",
+		"github": "https://github.com/GoldBigDragon/DFC2020_301_ARIA_RANSOMWARE",
+		"icon": "/resources/img/program/ARIA Algorithm Ransomware/icon.png",
+		"created-at": "2020-03-20",
+		"updated-at": "2020-06-23",
+	}
 ];
---
+++ resources/js/program.js
@@ -0,0 +1,10 @@
+function searchEnter(){
+}
+
+function search(){
+	const category = document.getElementById("search-category").value;
+	const value = document.getElementById("search-input").value;
+	const programArea = document.getElementById("programArea");
+}
+
+search();
