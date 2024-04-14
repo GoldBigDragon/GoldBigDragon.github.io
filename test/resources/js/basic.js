2d77b57bd2dc58f3ce3cf601edec6d4a7676ce76
GoldBigDragon <dnwndugod642@naver.com>
2024-04-14 22:59:19 +0900

    Daily touch

--- test/resources/js/basic.js
+++
@@ -1,8 +0,0 @@
-function copyClipboard(event) {
-	let copiedText = event.target.innerHTML;
-	navigator.clipboard.writeText(copiedText);
-}
-
-function copyClipboardText(text) {
-	navigator.clipboard.writeText(text);
-}
