e356734d1d3df868286804de5fe18364900be4c5
GoldBigDragon <dnwndugod642@naver.com>
2024-09-19 14:03:18 +0900

    Daily touch

--- resources/lang/service/ico-converter.js
+++ resources/lang/service/ico-converter.js
@@ -1,94 +1,94 @@
 const ICO_CONVERTER_LANG = {
 	"title": {
-		"en": "Image to ICO Converter (Drag & Drop)",
-		"kr": "Image to ICO Converter (Drag & Drop)",
-		"jp": "Image to ICO Converter (Drag & Drop)",
-		"cn": "Image to ICO Converter (Drag & Drop)",
-		"ru": "Image to ICO Converter (Drag & Drop)"
+		"en": "Image to ICO Converter",
+		"kr": "ICO 이미지 변환기",
+		"jp": "画像をICOに変換",
+		"cn": "图像转ICO转换器",
+		"ru": "Конвертер изображений в ICO"
 	},
 	"container": {
 		"en": "Drag & Drop your image files here or click to select files",
-		"kr": "Drag & Drop your image files here or click to select files",
-		"jp": "Drag & Drop your image files here or click to select files",
-		"cn": "Drag & Drop your image files here or click to select files",
-		"ru": "Drag & Drop your image files here or click to select files"
+		"kr": "변환할 파일을 드래그 드랍 하세요.",
+		"jp": "画像ファイルをここにドラッグ＆ドロップするか、クリックしてファイルを選択してください",
+		"cn": "将图像文件拖放到此处或单击以选择文件",
+		"ru": "Перетащите сюда файлы изображений или нажмите, чтобы выбрать файлы"
 	},
 	"absolute_size": {
-		"en": "Absolute Size",
-		"kr": "절대적 크기",
-		"jp": "Absolute Size",
-		"cn": "Absolute Size",
-		"ru": "Absolute Size"
+		"en": "<i class='fa-solid fa-up-right-and-down-left-from-center'></i>&nbsp;Absolute Size",
+		"kr": "<i class='fa-solid fa-up-right-and-down-left-from-center'></i>&nbsp;절대적 크기",
+		"jp": "<i class='fa-solid fa-up-right-and-down-left-from-center'></i>&nbsp;絶対サイズ",
+		"cn": "<i class='fa-solid fa-up-right-and-down-left-from-center'></i>&nbsp;绝对大小",
+		"ru": "<i class='fa-solid fa-up-right-and-down-left-from-center'></i>&nbsp;Абсолютный размер"
 	},
 	"relative_size": {
 		"en": "Relative Size",
 		"kr": "상대적 크기",
-		"jp": "Relative Size",
-		"cn": "Relative Size",
-		"ru": "Relative Size"
+		"jp": "相対サイズ",
+		"cn": "相对大小",
+		"ru": "Относительный размер"
 	},
 	"width": {
 		"en": "Width: ",
 		"kr": "가로: ",
-		"jp": "Width: ",
-		"cn": "Width: ",
-		"ru": "Width: "
+		"jp": "幅: ",
+		"cn": "宽度: ",
+		"ru": "Ширина: "
 	},
 	"height": {
 		"en": "Height: ",
 		"kr": "세로: ",
-		"jp": "Height: ",
-		"cn": "Height: ",
-		"ru": "Height: "
+		"jp": "高さ: ",
+		"cn": "高度: ",
+		"ru": "Высота: "
 	},
 	"percentage": {
 		"en": "Percentage: ",
 		"kr": "백분율: ",
-		"jp": "Percentage: ",
-		"cn": "Percentage: ",
-		"ru": "Percentage: "
+		"jp": "割合: ",
+		"cn": "百分比: ",
+		"ru": "Процент: "
 	},
 	"clear": {
 		"en": "Clear history",
 		"kr": "히스토리 제거",
-		"jp": "Clear history",
-		"cn": "Clear history",
-		"ru": "Clear history"
+		"jp": "履歴をクリア",
+		"cn": "清除历史",
+		"ru": "Очистить историю"
 	},
 	"download_zip": {
 		"en": "Download(.zip)",
 		"kr": "다운로드(.zip)",
-		"jp": "Download(.zip)",
-		"cn": "Download(.zip)",
-		"ru": "Download(.zip)"
+		"jp": "ダウンロード(.zip)",
+		"cn": "下载(.zip)",
+		"ru": "Скачать(.zip)"
 	},
 	"preview": {
 		"en": "Preview",
 		"kr": "미리보기",
-		"jp": "Preview",
-		"cn": "Preview",
-		"ru": "Preview"
+		"jp": "プレビュー",
+		"cn": "预览",
+		"ru": "Предварительный просмотр"
 	},
 	"filename": {
 		"en": "File Name",
 		"kr": "파일 이름",
-		"jp": "File Name",
-		"cn": "File Name",
-		"ru": "File Name"
+		"jp": "ファイル名",
+		"cn": "文件名",
+		"ru": "Имя файла"
 	},
 	"size": {
 		"en": "Size",
 		"kr": "파일 크기",
-		"jp": "Size",
-		"cn": "Size",
-		"ru": "Size"
+		"jp": "サイズ",
+		"cn": "大小",
+		"ru": "Размер"
 	},
 	"download": {
 		"en": "Download",
 		"kr": "다운로드",
-		"jp": "download",
-		"cn": "download",
-		"ru": "download"
+		"jp": "ダウンロード",
+		"cn": "下载",
+		"ru": "Скачать"
 	}
 }
 LANGUAGE_OBJECT["ICO_CONVERTER_LANG"] = ICO_CONVERTER_LANG;
