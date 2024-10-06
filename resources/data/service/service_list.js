d7dedb9ec9789319654eefbc52849ee196ad5ecb
GoldBigDragon <dnwndugod642@naver.com>
2024-10-06 23:54:37 +0900

    Daily touch

--- resources/data/service/service_list.js
+++ resources/data/service/service_list.js
@@ -1,22 +1,22 @@
 const SERVICE_LIST = [
 	{
-		"url": "/service/ico-converter.html",
+		"url": "/service/image-converter.html",
 		"name": {
-			"en": "ICO Converter",
-			"kr": "ICO 변환",
-			"jp": "ICOコンバーター",
-			"cn": "ICO 转换器",
-			"ru": "Конвертер ICO"
+			"en": "Image Converter",
+			"kr": "이미지 변환기",
+			"jp": "画像コンバーター",
+			"cn": "图像转换器",
+			"ru": "Конвертер изображений"
 		},
 		"description": {
-			"en": "Convert various image formats such as JPEG, PNG, etc. to ICO format.",
-			"kr": "JPEG, PNG 등 다양한 이미지 포맷을 ICO 포맷으로 변환합니다.",
-			"jp": "JPEGやPNGなど、さまざまな画像フォーマットをICOフォーマットに変換します。",
-			"cn": "将JPEG、PNG等各种图像格式转换为ICO格式。",
-			"ru": "Преобразование различных форматов изображений, таких как JPEG, PNG и других, в формат ICO."
+			"en": "You can convert image files to various formats such as JPEG, PNG, ICO, etc. or reduce their size.",
+			"kr": "이미지 파일을 JPEG, PNG, ICO 등 다양한 포맷으로 변환 시키거나 용량을 줄일 수 있습니다.",
+			"jp": "画像ファイルをJPEG、PNG、ICOなど、さまざまなフォーマットに変換したり、容量を減らしたりできます。",
+			"cn": "您可以将图像文件转换为各种格式，例如 JPEG、PNG、ICO 等或减小其大小。",
+			"ru": "Вы можете конвертировать файлы изображений в различные форматы, такие как JPEG, PNG, ICO и т. д., или уменьшать их размер."
 		},
-		"tag": ["image", "converter"],
-		"icon": "/resources/img/service/logo/ico-converter.png",
+		"tag": ["image", "convert", "compress"],
+		"icon": "/resources/img/service/logo/image-converter.png",
 		"created-at": "2024-10-06",
 		"updated-at": "2024-10-06"
 	}
---
+++ resources/lang/service/image-converter.js
@@ -0,0 +1,164 @@
+const ICO_CONVERTER_LANG = {
+	"title": {
+		"en": "Image Converter",
+		"kr": "이미지 변환기",
+		"jp": "画像コンバーター",
+		"cn": "图像转换器",
+		"ru": "Конвертер изображений"
+	},
+	"description": {
+		"en": "Uploaded and converted image files can only be downloaded by you and will not be shared anywhere.",
+		"kr": "업로드 및 변환 된 이미지 파일은 당신만 다운로드받을 수 있으며, 어디에도 공유되지 않습니다.",
+		"jp": "アップロードおよび変換された画像ファイルは、あなただけがダウンロードでき、どこにも共有されません。",
+		"cn": "上传和转换后的图像文件只能由您下载，不会在任何地方共享。",
+		"ru": "Загруженные и преобразованные файлы изображений можете скачать только вы и они не будут нигде опубликованы."
+	},
+	"container": {
+		"en": "Drag & Drop your image files here or click to select files",
+		"kr": "변환할 파일을 드래그 드랍 하세요.",
+		"jp": "画像ファイルをここにドラッグ＆ドロップするか、<br>クリックしてファイルを選択してください",
+		"cn": "将图像文件拖放到此处或单击以选择文件",
+		"ru": "Перетащите сюда файлы изображений или нажмите, чтобы выбрать файлы"
+	},
+	"absolute_size": {
+		"en": "Absolute Size",
+		"kr": "절대적 크기",
+		"jp": "絶対サイズ",
+		"cn": "绝对大小",
+		"ru": "Абсолютный размер"
+	},
+	"relative_size": {
+		"en": "Relative Size",
+		"kr": "상대적 크기",
+		"jp": "相対サイズ",
+		"cn": "相对大小",
+		"ru": "Относительный размер"
+	},
+	"width": {
+		"en": "Width: ",
+		"kr": "가로: ",
+		"jp": "幅: ",
+		"cn": "宽度: ",
+		"ru": "Ширина: "
+	},
+	"height": {
+		"en": "Height: ",
+		"kr": "세로: ",
+		"jp": "高さ: ",
+		"cn": "高度: ",
+		"ru": "Высота: "
+	},
+	"percentage": {
+		"en": "Percentage: ",
+		"kr": "백분율: ",
+		"jp": "割合: ",
+		"cn": "百分比: ",
+		"ru": "Процент: "
+	},
+	"clear": {
+		"en": "Clear history",
+		"kr": "히스토리 제거",
+		"jp": "履歴をクリア",
+		"cn": "清除历史",
+		"ru": "Очистить историю"
+	},
+	"download_zip": {
+		"en": "Download(.zip)",
+		"kr": "다운로드(.zip)",
+		"jp": "ダウンロード(.zip)",
+		"cn": "下载(.zip)",
+		"ru": "Скачать(.zip)"
+	},
+	"preview": {
+		"en": "Preview",
+		"kr": "미리보기",
+		"jp": "プレビュー",
+		"cn": "预览",
+		"ru": "Предварительный просмотр"
+	},
+	"filename": {
+		"en": "File Name",
+		"kr": "파일 이름",
+		"jp": "ファイル名",
+		"cn": "文件名",
+		"ru": "Имя файла"
+	},
+	"size": {
+		"en": "Size",
+		"kr": "파일 크기",
+		"jp": "サイズ",
+		"cn": "大小",
+		"ru": "Размер"
+	},
+	"download": {
+		"en": "Download",
+		"kr": "다운로드",
+		"jp": "ダウンロード",
+		"cn": "下载",
+		"ru": "Скачать"
+	},
+	"image_size": {
+		"en": "Size",
+		"kr": "크기",
+		"jp": "サイズ",
+		"cn": "大小",
+		"ru": "Размер"
+	},
+	"convert_format": {
+		"en": "Format",
+		"kr": "포맷",
+		"jp": "フォーマット",
+		"cn": "格式",
+		"ru": "Формат"
+	},
+	"image_compress": {
+		"en": "Compress",
+		"kr": "압축",
+		"jp": "圧縮",
+		"cn": "压缩",
+		"ru": "Сжать"
+	},
+	"compress_0": {
+		"en": "No compression",
+		"kr": "품질 유지",
+		"jp": "圧縮なし",
+		"cn": "不压缩",
+		"ru": "Без сжатия"
+	},
+	"compress_1": {
+		"en": "Slight compression",
+		"kr": "조금 압축",
+		"jp": "わずかに圧縮",
+		"cn": "稍微压缩",
+		"ru": "Небольшое сжатие"
+	},
+	"compress_2": {
+		"en": "Moderate compression",
+		"kr": "적당히 압축",
+		"jp": "適度に圧縮",
+		"cn": "适度压缩",
+		"ru": "Умеренное сжатие"
+	},
+	"compress_3": {
+		"en": "Compressed",
+		"kr": "압축",
+		"jp": "圧縮済み",
+		"cn": "已压缩",
+		"ru": "Сжато"
+	},
+	"compress_4": {
+		"en": "Heavily compressed",
+		"kr": "많이 압축",
+		"jp": "高圧縮",
+		"cn": "高度压缩",
+		"ru": "Сильно сжато"
+	},
+	"compress_5": {
+		"en": "Maximum compression",
+		"kr": "압축률 최대",
+		"jp": "最大圧縮",
+		"cn": "最大压缩",
+		"ru": "Максимальное сжатие"
+	}
+}
+LANGUAGE_OBJECT["ICO_CONVERTER_LANG"] = ICO_CONVERTER_LANG;
